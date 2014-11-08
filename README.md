f8535fec93bf317afdc472bb3642eb04b4f0491e
Lajos Gerecs <lajos.gerecs@minify.hu>
2014-11-08 20:24:37 +0000

    ready

--- README.md
+++ README.md
@@ -3,7 +3,29 @@ brainfuck-haskell
 
 A brainfuck parser implemented in haskell
 
-It is not fully functioning yet, nested loops are not working
+Finally nested loops are working!
+
+Sierpinski generated into sierpinski_output.txt
+
+If you want to run your own code:
+
+```
+
+ghci brainfuck.hs 
+
+let s = newState "brainfuck code"
+run s
+
+```
+
+if you want to run step by step you can run:
+
+```
+let nextState = step state
+
+```
+
+This was a learning excercise for me, code may not be the best. 
 
 
 
--- brainfuck.hs
+++ brainfuck.hs
@@ -2,6 +2,8 @@ import Data.Char
 import Data.List
 import Data.Array.IArray
 import qualified Test.QuickCheck as QC
+import System.IO
+
 
 zeroMemory = 0 : zeroMemory
 
@@ -64,14 +66,14 @@ step state = state{codePtr = ( (codePtr state) + 1)}
 -- if the byte at the data pointer is zero, jump it forward to the command after the matching ]
 jumpRightIfNeeded :: State -> State
 jumpRightIfNeeded state = if (isZero (dataPtr state) (memory state)) then 
-        jumpTo (nextClosingBracket (codes state) (codePtr state)) state
+        jumpTo (findLoopEnd (codes state) (codePtr state)) state
     else
         step state
 
 -- if the byte at the data pointer is nonzero, jump it back to the command after the matching [ command
 jumpLeftIfNeeded :: State -> State
 jumpLeftIfNeeded state = if (not (isZero (dataPtr state) (memory state))) then 
-        jumpTo ((prevOpeningBracket (codes state) (codePtr state)) + 1) state
+        jumpTo (findLoopStart (codes state) (codePtr state)) state
     else
         step (loopEnded state)
 
@@ -111,12 +113,6 @@ finished state = (codePtr state) >= length (codes state)
 isZero :: Int -> [Int]  -> Bool 
 isZero index list = list!!index == 0
 
-nextClosingBracket :: String -> Int -> Int
-nextClosingBracket code currentIndex =  minimum(filter ( > currentIndex) (elemIndices ']' code))
-
-prevOpeningBracket :: String -> Int -> Int
-prevOpeningBracket code currentIndex = maximum(filter ( < currentIndex) (elemIndices '[' code))
-
 
 setMemValue :: [Int] -> Int -> Int -> [Int]
 setMemValue memory dataPtr value = replace dataPtr ( mod value 255 ) memory
@@ -137,21 +133,42 @@ findLoopStart codes current =
 
 findLoopEnd :: [Char] -> Int -> Int
 findLoopEnd codes current = 
-    countUntilLoopsClosed codes (current - 1) 0 0 (1)
+    countUntilLoopsClosed codes (current + 1) 0 0 (1)
+
+loopMarker direction = if (direction < 0) then '[' else ']'
 
 -- count bakcwards or forward while for current loop start/end point
 countUntilLoopsClosed :: [Char] -> Int -> Int -> Int -> Int -> Int
-countUntilLoopsClosed codes current opened closed direction = if (codes!!current == '[' && (opened - closed) == 0) then
+countUntilLoopsClosed codes current opened closed direction = if (codes!!current == (loopMarker direction) && (opened - closed) == 0) then
         current
     else (
         case codes!!current of 
-            ']' -> countUntilLoopsClosed codes (current + direction) (closed + 1) (opened) direction
-            '[' -> countUntilLoopsClosed codes (current + direction) (closed) (opened + 1) direction
-            otherwise -> countUntilLoopsClosed codes (current - 1) (closed) (opened) direction
+            ']' -> countUntilLoopsClosed codes (current + direction) (opened) (closed + 1) direction
+            '[' -> countUntilLoopsClosed codes (current + direction) (opened + 1) (closed) direction
+            otherwise -> countUntilLoopsClosed codes (current + direction) (opened) (closed) direction
         )
 
 testCountBackwards :: Bool
 testCountBackwards =
     ((countUntilLoopsClosed "[]" 0 0 0 (-1) ) == 0) &&
+    ((countUntilLoopsClosed "[a]" 1 0 0 (-1) ) == 0) &&
     ((countUntilLoopsClosed ">>[abc]<<" 5 0 0 (-1)) == 2) &&
     ((countUntilLoopsClosed "abc[a[bcd]e[f]g]9" 14 0 0 (-1)) == 3)
+
+
+testLoopStartFind :: Bool 
+testLoopStartFind =
+    (findLoopStart "a[b[c[d[e]f]g]h]i" 15) == 1 &&
+    (findLoopStart "[[]]a[[]]" 8) == 5 && 
+    (findLoopStart "[]" 1) == 0 &&
+    (findLoopStart "[[]]" 3) == 0 && 
+    True
+
+testLoopEndFind :: Bool
+testLoopEndFind = 
+    (findLoopEnd "a[b[c[d[e]f]g]h]i" 1) == 15 && True
+
+writeResultToFile state fileName = do
+    outh <- openFile fileName WriteMode
+    let res = run state
+    hPutStrLn outh (output res)
